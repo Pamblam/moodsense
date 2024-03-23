d3df3f958db3b35793695c202b3b13ba69e9ee37
Pamblam <adelphia@protonmail.ch>
2024-03-23 17:29:52 -0400

    Saivng

--- public/js/index.js
+++ public/js/index.js
@@ -4,7 +4,7 @@
 	let container = document.querySelector('div');
 
 	container.innerHTML = `
-	<img src='./img/logo.png' class='img-fluid' style='margin: 0 auto;' />
+	<img src='./img/logo.png' class='img-fluid' style='margin: 0 auto; max-width: 80%;' />
 	<div class='container'>
 		<div id='calendar'></div>
 		<div id='entries'></div>
@@ -35,6 +35,32 @@
 		'ff0000'
 	];
 
+	let emojis = [
+		'😄',
+		'😃',
+		'😀',
+		'🙂',
+		'😐',
+		'😕',
+		'🙁',
+		'😟',
+		'😞',
+		'😫'
+	];
+
+	let mood_adjectives = [
+		'really good',
+		'pretty good',
+		'good',
+		'decent',
+		'neutral',
+		'poor',
+		'bad',
+		'pretty bad',
+		'really bad',
+		'terrible'
+	];
+
 	let cal_opts = {
 		abbrDay: true,
 		onDayClick(date){
@@ -43,18 +69,31 @@
 			let d = `${date.getFullYear()}-${month}-${day}`;
 			console.log(d);
 			if(DATA[d]){
-				let mood_face = DATA[d].avg > 7 ? '😡' : (DATA[d].avg > 3 ? '😐' : '😄');
-				let mood_text = DATA[d].avg > 7 ? 'bad' : (DATA[d].avg > 3 ? 'neutral' : 'good');
+				let scale = Math.round(DATA[date].avg)-1;
+				let mood_face = emojis[scale];
+				let mood_text = mood_adjectives[scale];
 
-				entries_ele.innerHTML = `
-					<p class='text-center'><b>${DATA[d].entries.length} entr${DATA[d].entries.length===1?'y':'ies'} on ${d}</b></p>
-					<p class='text-center'><small>${mood_face} Your over-all mood was ${mood_text} on this day (${DATA[d].avg} out of 10).</small></p>
-					${DATA[d].entries.map(entry=>{
-						return `<div class='econtainer'>
-							<div class='entry-box'>${entry.entry}</div>
-							<div class='entry-box'>${entry.response}</div>
-						</div>`;
-					}).join('')}
+				entries_ele.innerHTML = `<div class='m-3'>
+						<p class='text-center'><b>${DATA[d].entries.length} entr${DATA[d].entries.length===1?'y':'ies'} on ${d}</b></p>
+						<p class='text-center'><small>${mood_face} Your over-all mood was <b>${mood_text}</b> on this day (${DATA[d].avg} out of 10).</small></p>
+						<hr>
+						${DATA[d].entries.map(entry=>{
+							var dte = new Date().toLocaleTimeString();
+							var p = dte.split(/\D/);
+							var mer = dte.split(' ').pop();
+							let time = `${p[0]}:${p[1]} ${mer}`;
+
+							let scale = Math.round(entry.rating)-1;
+							let mood_face = emojis[scale];
+							let mood_text = mood_adjectives[scale];
+
+							return `<div class='econtainer'>
+								<div><b>${time}</b> (Mood: ${mood_face} ${entry.rating}/10)</div>
+								<div class='entry-box'>${entry.entry}</div>
+								<div class='entry-box text-muted'><small><i><b>Response:</b> ${entry.response}</i></small></div>
+							</div>`;
+						}).join('')}
+					</div>
 				`;
 			}else{
 				entries_ele.innerHTML = `<p><b>No entries on ${d}</b></p>`;
