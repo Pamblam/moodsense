b6de0257c890a3e054d5fd034c5e9fab84651396
Pamblam <adelphia@protonmail.ch>
2024-03-23 12:22:11 -0400

    SAving

--- public/webhook.php
+++ public/webhook.php
@@ -1,8 +1,33 @@
 <?php
 
-$record = [
-	'GET' => $_GET,
-	'POST' => $_POST
-];
+/**
+ * Post Paramters:
+	'MessageSid' => '15f11ce2-f233-46ea-9283-54964411f1f2',
+	'SmsSid' => '15f11ce2-f233-46ea-9283-54964411f1f2',
+	'AccountSid' => '5eb0b49d-98a0-4b9f-a722-69d80b7fb7ca',
+	'From' => '+18137035515',
+	'To' => '+12179616663',
+	'Body' => 'Bop',
+	'NumMedia' => '0',
+	'NumSegments' => '1',
+ */
 
-file_put_contents(realpath(dirname(__FILE__))."/test.txt", var_export($record, true));
+if(!empty($_REQUEST['Body']) && !empty($_REQUEST['From'])){
+
+	$host = 'localhost';
+	$db   = 'moods';
+	$user = 'root';
+	$pass = 'bayhacks';
+	$charset = 'utf8mb4';
+
+	$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+	$options = [
+		PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+		PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+		PDO::ATTR_EMULATE_PREPARES   => false,
+	];
+	$pdo = new PDO($dsn, $user, $pass, $options);
+
+	
+
+}
