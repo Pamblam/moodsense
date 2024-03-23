138ae8dfa259865e03ffcdd029b4ab04b8519c65
Pamblam <adelphia@protonmail.ch>
2024-03-23 10:25:48 -0400

    Added variables

--- public/sendSMS.php
+++ public/sendSMS.php
@@ -2,16 +2,25 @@
 
 use SignalWire\Rest\Client;
 
-define('SW_SPACE_URL', '..');
-define('SW_PHONE_NUM', '..');
+require "../vendor/autoload.php";
 
-$client = new Client($PROJECT_ID, $API_TOKEN, [
+define('SW_SPACE_URL', 'moodsense.signalwire.com');
+define('SW_PHONE_NUM', '+12179616663');
+define('TO_NUMBER', '+18137035515');
+define('PROJECT_ID', '5eb0b49d-98a0-4b9f-a722-69d80b7fb7ca');
+define('API_TOKEN', 'PTb15822179028fc2921d991ec4de4b91f53b7361c2f76afcc');
+
+
+
+$body = "Hey there";
+
+$client = new Client(PROJECT_ID, API_TOKEN, [
 	"signalwireSpaceUrl" => SW_SPACE_URL
 ]);
 
-$message = $client->messages->create($TO_NUMBER, [
-	"from" => $SW_PHONE_NUM, 
-	"body" => "Hello"
+$message = $client->messages->create(TO_NUMBER, [
+	"from" => SW_PHONE_NUM, 
+	"body" => $body
 ]);
 
 print($message->sid);
