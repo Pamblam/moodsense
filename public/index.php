c10ba5fe57b89c7f5825ce7616472be5c1c0e600
Pamblam <adelphia@protonmail.ch>
2024-03-24 12:47:42 -0400

    Saving

--- public/index.php
+++ public/index.php
@@ -17,8 +17,8 @@
 				<img src="./img/logo.png" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy">
 			</div>
 			<div class="col-lg-6">
-				<h1 class="display-5 fw-bold lh-1 mb-3">MoodSense</h1>
-				<p class="lead">Express your feelings, receive valuable feedback, and document your journey over time.</p>
+				<h1 class="display-5 fw-bold lh-1 mb-3">Your AI Therapist</h1>
+				<p class="lead">Express your feelings, receive valuable feedback, and document your journey over time with MoodSense. Nothing do download, no apps to open, simply send a text and receive instant, actionable feedback.</p>
 				<div class='text-center'>
 					<small>Powered by</small>
 					<img src='./img/signalwire.png' class='img-fluid' style='max-width: 200px; margin:0 auto;' />
@@ -30,7 +30,7 @@
 	<div class="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center">
 		<div class="col-lg-7 p-3 p-lg-5 pt-lg-3">
 			<h3 class="display-4 fw-bold lh-1">Get Instant Feedback</h3>
-			<p class="lead">Your AI companion assesses your thoughts and provides helpful observations and suggestions.</p>
+			<p class="lead">Your AI companion assesses your thoughts and provides helpful observations and suggestions. With MoodSense, conveninece is key. Everyone can benefit from therapy, but feelings are often fleeting. MoodSense can suppliment therapy by tracking your feelings and giving you feedback in real time.</p>
 		</div>
 		<div class="col-lg-4 offset-lg-1 p-0 overflow-hidden shadow-lg">
 			<img class="rounded-lg-3" src="./img/chat-ss.jpg" alt="" width="720">
@@ -43,31 +43,23 @@
 		</div>
 		<div class="col-lg-7 p-3 p-lg-5 pt-lg-3">
 			<h3 class="display-4 fw-bold lh-1">Track Your Feelings</h3>
-			<p class="lead">The calendar feature is an easy and convenient space to access records of your thoughts and experiences, download-free.</p>
+			<p class="lead">The calendar feature is an easy and convenient space to access records of your thoughts and experiences, download-free. Simply send "calendar" to your MoodSense companion and get a link to your personalized, color-coded calendar. The calendar days are rendered on a gradient scale with the bluer days being good days, and days on the red-end of the spectrum being bad days.</p>
 		</div>
 	</div>
 
 	<div class="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center">
 		
 		<div class="col-lg-7 p-3 p-lg-5 pt-lg-3">
-			<h3 class="display-4 fw-bold lh-1">Track Your Feelings</h3>
-			<p class="lead">The calendar feature is an easy and convenient space to access records of your thoughts and experiences, download-free.</p>
+			<h3 class="display-4 fw-bold lh-1">Written for TADHack 2024!</h3>
+			<p class="lead">Moodsense runs on a LAMP stack and is powered by TADHack's sponsor, SignalWire. Pam Mishaw handled the PHP code while Rob Parham set up the server and did some Javascript development. The project's fully working source code is available for review on <a href='https://github.com/Pamblam/moodsense' style='white-space:nowrap; text-decoration: none;'><i class="fa-brands fa-github"></i> Github</a>.</p>
+			<img src='./img/photo.png' style=' width: 300px; max-width:100%; margin: 0 auto; display: block;'>
 		</div>
 		<div class="col-lg-4 offset-lg-1 p-0 overflow-hidden">
 			<iframe width="315" height="560" src="https://www.youtube.com/embed/ngGVmXPb2PU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 		</div>
 	</div>
 
-	<div class="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center">
-		<div class="col-lg-4 offset-lg-1 p-0 overflow-hidden">
-			<img class="rounded-lg-3 img-fluid" src="./img/tadhack.png" alt="" style='max-width:100%; max-height: 80vh;'>
-		</div>
-		<div class="col-lg-7 p-3 p-lg-5 pt-lg-3">
-			<h5 class="display-4 fw-bold lh-1">Written for Tadhack!</h5>
-			<p class="lead">Written by Rob and Pam for TadHacks... </p>
-			<p>Check out the repo on <a href='https://github.com/Pamblam/moodsense'><i class="fa-brands fa-github"></i> Github!</a></a>
-		</div>
-	</div>
+	<br><br>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 	<script src="./js/index.js"></script>
