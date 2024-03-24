d63264efe907d508074f966b0f93fc6cefbdbcc3
Pamblam <adelphia@protonmail.ch>
2024-03-23 22:28:37 -0400

    SAving

--- public/index.php
+++ public/index.php
@@ -12,6 +12,7 @@
 			<img src='./img/logo.png' class='img-fluid' style='margin: 0 auto; max-width: 80%;' />
 		</div>
         <h1 class='text-center'>Welcome to moodsense.edv</h1>
+        <a href='https://github.com/Pamblam/moodsense/'>Visit the project GitHub</a>
 	</div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 	<script src="./js/index.js"></script>
