2531fca252f3578c548f332ffc09868e3b5e5e07
Pamblam <adelphia@protonmail.ch>
2024-03-24 10:16:34 -0400

    Saving

--- public/index.php
+++ public/index.php
@@ -5,15 +5,59 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Mood Sense</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+	<link href="./css/website.css" rel="stylesheet">
   </head>
   <body>
-    <div class='container'>
-		<div class='text-center m-5'>
-			<img src='./img/logo.png' class='img-fluid' style='margin: 0 auto; max-width: 80%;' />
+    
+
+	<div class="container col-xxl-8 px-4 py-5">
+		<div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+			<div class="col-10 col-sm-8 col-lg-6">
+				<img src="./img/logo.png" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy">
+			</div>
+			<div class="col-lg-6">
+				<h1 class="display-5 fw-bold lh-1 mb-3">MoodSense</h1>
+				<p class="lead">This is a reallly cool description and i love this product and you should love it too. and i lvoe pam. and her buns..</p>
+				<div class='text-center'>
+					<small>Powered by</small>
+					<img src='./img/signalwire.png' class='img-fluid' style='max-width: 200px; margin:0 auto;' />
+				</div>
+			</div>
 		</div>
-        <h1 class='text-center'>Welcome to moodsense.edv</h1>
-        <a href='https://github.com/Pamblam/moodsense/'>Visit the project GitHub</a>
 	</div>
+        
+	<div class="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center">
+		<div class="col-lg-7 p-3 p-lg-5 pt-lg-3">
+			<h3 class="display-4 fw-bold lh-1">Get instant Feedback</h3>
+			<p class="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+		</div>
+		<div class="col-lg-4 offset-lg-1 p-0 overflow-hidden shadow-lg">
+			<img class="rounded-lg-3" src="./img/chat-ss.jpg" alt="" width="720">
+		</div>
+	</div>
+
+	<div class="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center">
+		<div class="col-lg-4 offset-lg-1 p-0 overflow-hidden">
+			<img class="rounded-lg-3 img-fluid" src="./img/cal-ss.jpg" alt="" style='max-width:100%; max-height: 80vh;'>
+		</div>
+		<div class="col-lg-7 p-3 p-lg-5 pt-lg-3">
+			<h3 class="display-4 fw-bold lh-1">Track your feelings</h3>
+			<p class="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+		</div>
+	</div>
+
+	<div class="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center">
+		<div class="col-lg-4 offset-lg-1 p-0 overflow-hidden">
+			<img class="rounded-lg-3 img-fluid" src="./img/tadhack.png" alt="" style='max-width:100%; max-height: 80vh;'>
+		</div>
+		<div class="col-lg-7 p-3 p-lg-5 pt-lg-3">
+			<h5 class="display-4 fw-bold lh-1">Written for Tadhack!</h5>
+			<p class="lead">Written by Rob and Pam for TadHacks... </p>
+			<p>Check out the repo on <a href='https://github.com/Pamblam/moodsense'><i class="fa-brands fa-github"></i> Github!</a></a>
+		</div>
+	</div>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 	<script src="./js/index.js"></script>
   </body>
